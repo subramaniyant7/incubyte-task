58545ba4cc645ef6b787a5e0de335c264c45e131
subramaniyant7 <72293520+subramaniyant7@users.noreply.github.com>
2025-04-13 08:03:57 +0530

    Step 4: Support custom delimiters
    Step 4: Support custom delimiters
    
    Now let's add support for custom delimiters.

--- task.js
+++ task.js
@@ -1,8 +1,19 @@
 function add(numbers) {
     if (!numbers) return 0;
-    return numbers.split(/[,\n]/)
-                 .map(num => parseInt(num, 10))
-                 .reduce((sum, num) => sum + num, 0);
+    
+    let delimiter = /[,\n]/;
+    let nums = numbers;
+    
+    // Check for custom delimiter
+    if (numbers.startsWith('//')) {
+        const delimiterEnd = numbers.indexOf('\n');
+        delimiter = numbers.substring(2, delimiterEnd);
+        nums = numbers.substring(delimiterEnd + 1);
+    }
+    
+    return nums.split(delimiter)
+               .map(num => parseInt(num, 10))
+               .reduce((sum, num) => sum + num, 0);
 }
 
 // Tests
@@ -19,3 +30,8 @@ console.log(add("10,20,5") === 35, "'10,20,5' should return 35");
 // Step 3
 console.log(add("1\n2,3") === 6, "'1\\n2,3' should return 6");
 console.log(add("10\n20\n5") === 35, "'10\\n20\\n5' should return 35");
+
+// Step 4
+console.log(add("//;\n1;2") === 3, "Custom delimiter ';' should work");
+console.log(add("//|\n1|2|3") === 6, "Custom delimiter '|' should work");
+console.log(add("//sep\n2sep5") === 7, "Custom delimiter 'sep' should work");
