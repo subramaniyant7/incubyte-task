08f4fedc9caa3befcf1678df866507085278abcd
subramaniyant7 <72293520+subramaniyant7@users.noreply.github.com>
2025-04-13 08:02:07 +0530

    Step 3: Handle new lines as separators
    Step 3: Handle new lines as separators
    
    Now let's handle new lines between numbers.

--- task.js
+++ task.js
@@ -1,6 +1,6 @@
 function add(numbers) {
     if (!numbers) return 0;
-    return numbers.split(',')
+    return numbers.split(/[,\n]/)
                  .map(num => parseInt(num, 10))
                  .reduce((sum, num) => sum + num, 0);
 }
@@ -15,3 +15,7 @@ console.log(add("5") === 5, "'5' should return 5");
 // Step 2
 console.log(add("1,2") === 3, "'1,2' should return 3");
 console.log(add("10,20,5") === 35, "'10,20,5' should return 35");
+
+// Step 3
+console.log(add("1\n2,3") === 6, "'1\\n2,3' should return 6");
+console.log(add("10\n20\n5") === 35, "'10\\n20\\n5' should return 35");
